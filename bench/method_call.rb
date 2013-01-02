48100b2f83403fce2f1d4d0c25be538060e57262
Sho Kusano <rosylilly@aduca.org>
2013-01-03 08:07:42 +0900

    Fix benchmark

--- bench/method_call.rb
+++ bench/method_call.rb
@@ -1,15 +1,31 @@
 require 'bundler'
 Bundler.require(:default, :development)
+require 'forwardable'
 
 class Example
   def hi; 1; end
+  def hello; '1'; end
 end
 
 class ExampleDelegator < SimpleDelegator
   def hi; 2; end
 end
 
+class ExampleForwardable
+  extend Forwardable
+
+  def_delegator :@example, :hello
+
+  def initialize(example)
+    @example = example
+  end
+
+  def hi; 3; end
+end
+
 module ExampleMixin
+  include Dicer::Behavior
+
   def hi; 3; end
 end
 
@@ -22,21 +38,31 @@ Benchmark.ips do |bench|
   bench.report('without Mixin') do
     example = Example.new
     example.hi
+    example.hello
   end
 
   bench.report('with Delegate') do
     example = ExampleDelegator.new(Example.new)
     example.hi
+    example.hello
+  end
+
+  bench.report('with Forwardable') do
+    example = ExampleForwardable.new(Example.new)
+    example.hi
+    example.hello
   end
 
   bench.report('with Mixin') do
     example = Example.new
     example.extend(ExampleMixin)
     example.hi
+    example.hello
   end
 
   bench.report('with Dicer') do
     example = example_description.delegator.new(Example.new)
     example.hi
+    example.hello
   end
 end
