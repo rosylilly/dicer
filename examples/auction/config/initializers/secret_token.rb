0bf72adcc640ae99b9f6eac76cc3a1f8cb339af1
Sho Kusano <rosylilly@aduca.org>
2013-01-20 03:14:01 +0900

    New Sample: auction

---
+++ examples/auction/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Auction::Application.config.secret_token = '181f76fbe3ac7ff2355a5eae5ac8b05728fdf7913240a345c9fd78e7b574d9252605dc0c446dac46b6dccf518de49940f00f2e750aa84ad41218ee40a3ddc824'
