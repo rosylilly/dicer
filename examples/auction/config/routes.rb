e1b300130d5c47517ea8168d0910d0f2f6a9f0fd
Sho Kusano <rosylilly@aduca.org>
2013-01-27 01:05:30 +0900

    Implement Authentication#sign_in, #sign_out, #log_in

--- examples/auction/config/routes.rb
+++ examples/auction/config/routes.rb
@@ -3,6 +3,7 @@ Auction::Application.routes.draw do
 
   get 'sign_in' => 'authentication#sign_in'
   get 'sign_up' => 'authentication#sign_up'
+  post 'log_in' => 'authentication#log_in'
   get 'sign_out' => 'authentication#sign_out'
 
   resources :users, only: [:show, :create]
