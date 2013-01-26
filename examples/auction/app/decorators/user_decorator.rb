0e4b54ddbce2e495f737bb9f7905c438d094465c
Sho Kusano <rosylilly@aduca.org>
2013-01-27 03:20:26 +0900

    Replace relative time method

--- examples/auction/app/decorators/user_decorator.rb
+++ examples/auction/app/decorators/user_decorator.rb
@@ -2,6 +2,10 @@ class UserDecorator < Draper::Base
   decorates :user
 
   def created_at
-    %Q{<time datetime="#{source.created_at.to_s(:rfc822)}\">#{source.created_at.to_s(:relative)}</time>}.html_safe
+    %Q{<time datetime="#{
+      source.created_at.to_s(:rfc822)
+    }\">#{
+      h.distance_of_time_in_words(Time.now, source.created_at)
+    }</time>}.html_safe
   end
 end
